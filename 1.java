31c1082d12997199422b430d39fb40ad31ccb5bf
marcos0180 <153757966+marcos0180@users.noreply.github.com>
2024-02-13 23:11:36 +0530

    1.java

---
+++ 1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Ex {
+    public static void main(String args[]) {
+        double perc;
+        int temp = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter percentage: ");
+        perc = sc.nextDouble();
+
+        if (perc > 70) {
+            System.out.println("Grade A");
+        } else if (perc > 60 && perc <= 70) {
+            System.out.println("Grade B");
+        } else if (perc > 50 && perc <= 60) {
+            System.out.println("Grade C");
+        } else {
+            System.out.println("Grade F");
+        }
+
+    }
+}
