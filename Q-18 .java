c73b0ed7b092a7a85720b1d7c1942c4ec79bf8bf
marcos0180 <153757966+marcos0180@users.noreply.github.com>
2024-02-16 06:53:47 +0530

    Q-18 .java

---
+++ Q-18 .java
@@ -0,0 +1,17 @@
+//Q-18
+
+import java.util.*;
+
+class Ex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter year: ");
+        int a = sc.nextInt();
+
+        if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0)) {
+            System.out.println(a + " is a leap year.");
+        } else {
+            System.out.println(a + " is not a leap year.");
+        }
+    }
+}
